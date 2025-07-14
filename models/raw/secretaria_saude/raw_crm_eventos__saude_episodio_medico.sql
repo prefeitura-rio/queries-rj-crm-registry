a197eb160b1226c7a5470655ecf5c5fe005fbdf2
ThiagoTrabach <thiago.trabach@gmail.com>
2025-07-14 14:52:28 -0300

    refact: simplify SQL view by removing unnecessary CTEs in 'saude__episodio_medico'

--- models/raw/secretaria_saude/raw_crm_eventos__saude_episodio_medico.sql
+++ models/raw/secretaria_saude/raw_crm_eventos__saude_episodio_medico.sql
@@ -1,33 +1,8 @@
 {{ config(alias="saude__episodio_medico", schema="crm_eventos", materialized="view") }}
 with
-    source as (select * from {{ source("rj-sms-historico-clinico", "episodio_assistencial") }}),
-    renamed as (
-        select
-            id_hci,
-            paciente_cpf,
-            cadastros_conflitantes_indicador,
-            paciente,
-            tipo,
-            subtipo,
-            entrada_data,
-            entrada_datahora,
-            saida_datahora,
-            exames_realizados,
-            procedimentos_realizados,
-            medidas,
-            motivo_atendimento,
-            desfecho_atendimento,
-            obito_indicador,
-            condicoes,
-            prescricoes,
-            medicamentos_administrados,
-            estabelecimento,
-            profissional_saude_responsavel,
-            prontuario,
-            metadados,
-            data_particao
-
-        from source
+    source as (
+        select * from {{ source("rj-sms-historico-clinico", "episodio_assistencial") }}
     )
+
 select *
-from renamed
+from source
